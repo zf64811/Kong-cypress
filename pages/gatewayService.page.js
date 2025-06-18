9f5767c61e3bbd7e0032387b79dd21b47432ab53
zf64811 <monky.d.fzy@gmail.com>
2025-06-18 23:50:21 +0800

    [UPDATE] github action test

--- pages/gatewayService.page.js
+++ pages/gatewayService.page.js
@@ -1,6 +1,6 @@
 class GatewayServicePage {
   get addNewgatewayService_Button() {
-    return cy.get('[data-testid="toolbar-add-gateway-service"]');
+    return cy.get('[data-testid="empty-state-action"]');
   }
   get newgatewayServiceName_Input() {
     return cy.get('[data-testid="gateway-service-name-input"');
