2d1dcce46435b0073b7ca9f849f74edc14a6c54e
zf64811 <monky.d.fzy@gmail.com>
2025-06-18 19:38:31 +0800

    [ADD] config reporter

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,19 @@
-const { defineConfig } = require("cypress");
+const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
+  },
+  reporter: 'cypress-mochawesome-reporter', // 使用 Mochawesome 自定义报告
+  reporterOptions: {
+    reportDir: 'cypress/reports',
+    charts: true,
+    reportPageTitle: 'Kong Manager Test Report',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
   },
 });
