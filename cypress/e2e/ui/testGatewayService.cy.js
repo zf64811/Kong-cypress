2bfa1b7f774b37f2b02e6d5b18e18150e262f1a6
zf64811 <monky.d.fzy@gmail.com>
2025-06-20 13:22:42 +0800

    [ADD] Test Case for delete gateway service

--- cypress/e2e/ui/testGatewayService.cy.js
+++ cypress/e2e/ui/testGatewayService.cy.js
@@ -7,39 +7,38 @@ const homePage = new HomePage();
 const overViewPage = new OverViewPage();
 
 describe('Test Add GatewayService', () => {
-    let getGatewayServicelistAPI;
+    let gatewayServicelistAPI;
     let serviceName, tag, invalidApi, validaApi;
     let workspacesTitle, overViewPageTitle;
 
     before(() => {
         cy.fixture('gatewayData').then(data => {
             ({ serviceName, tag, invalidApi, validaApi } = data.gatewayService);
-            ({ getGatewayServicelistAPI } = data.gatewayAPI);
+            ({ gatewayServicelistAPI } = data.gatewayAPI);
             ({ workspacesTitle, overViewPageTitle } = data.contextAssert);
         });
     });
 
     beforeEach(() => {
-        cy.intercept('GET', getGatewayServicelistAPI).as('getGatewayServicelist');
+        cy.intercept('GET', gatewayServicelistAPI).as('gatewayServicelist');
         cy.workspacesLogin();
         homePage.workspacesTitle.should('be.visible').and('contain', workspacesTitle);
         homePage.workspaceName.click();
         overViewPage.OverViewPageTitle.should('be.visible').and('contain', overViewPageTitle);
+        homePage.GatewayServiceTab.click();
+        cy.wait('@gatewayServicelist');
     });
 
     it('Url should be vaild when add a new gateway service', () => {
-        homePage.GatewayServiceTab.click();
-        cy.wait('@getGatewayServicelist');
         cy.addNewgatewayService(serviceName, tag, invalidApi);
         gatewayServicePage.gatewayServiceAlert_Message
             .should('be.visible')
             .and('contain', "Failed to construct 'URL': Invalid URL");
     });
 
-    it('Add a new gateway service', () => {
-        homePage.GatewayServiceTab.click();
-        cy.wait('@getGatewayServicelist');
+    it('Add a new gateway service successfully', () => {
         cy.addNewgatewayService(serviceName, tag, validaApi);
+        cy.wait(1000);
         gatewayServicePage.gatewayServicePrompt_Message
             .should('be.visible')
             .and('contain', `Gateway Service "${serviceName}" successfully created!`);
@@ -47,11 +46,25 @@ describe('Test Add GatewayService', () => {
     });
 
     it('gatewayService name should be unique when add a new gateway service', () => {
-        homePage.GatewayServiceTab.click();
-        cy.wait('@getGatewayServicelist');
         cy.addNewgatewayService(serviceName, tag, validaApi);
         gatewayServicePage.gatewayServiceAlert_Message
             .should('be.visible')
             .and('contain', `UNIQUE violation detected on '{name="${serviceName}"}'`);
+    });
+
+    it('Delete a new gateway service', () => {
+        gatewayServicePage.gatewayServiceFilter_Button.click();
+        gatewayServicePage.filterbyName_Button.click();
+        gatewayServicePage.filterbyName_Input.should('be.visible').type(serviceName);
+        gatewayServicePage.filterApply_Button.click();
+        cy.wait(1000);
+        gatewayServicePage.actionButton.should('be.visible').click();
+        gatewayServicePage.delete_Button.should('be.visible').click();
+        gatewayServicePage.confirmDelete_Input.should('be.visible').type(serviceName);
+        gatewayServicePage.confirmDelete_Input.should('have.value', serviceName);
+        gatewayServicePage.confirmDelete_button.should('be.visible').click();
+        gatewayServicePage.gatewayServicePrompt_Message
+            .should('be.visible')
+            .and('contain', `Gateway Service "${serviceName}" successfully deleted!`);
     });
 });
